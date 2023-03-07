540d579e2f5011222c4d5fe74646a5d954375ac8
WWangxiaoyu <461088277@qq.com>
2023-03-07 18:17:44 +0800

    Feign的最佳实践

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -14,16 +15,26 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
+    private UserClient userClient;
+
+    @Autowired
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        //2. 利用RestTemplate发起http请求，查询用户
-        //2.1 url路径
-        String url = "http://userservice/user/" + order.getUserId();
-        //2.2 发送http请求，实现远程调用
-        User user = restTemplate.getForObject(url, User.class);
+
+        //使用feign实现调用
+        User user = userClient.findById(order.getUserId());
+
+
+//        //2. 利用RestTemplate发起http请求，查询用户
+//        //2.1 url路径
+//        String url = "http://userservice/user/" + order.getUserId();
+//        //2.2 发送http请求，实现远程调用
+//        User user = restTemplate.getForObject(url, User.class);
+
+
         //3.封装User到Order
         order.setUser(user);
         // 4.返回
